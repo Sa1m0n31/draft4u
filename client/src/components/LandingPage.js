5dd824edf2505207d1e7fd902c4c91f945bbf44e
Szymon Burak <sajmon0031@gmail.com>
2021-12-10 01:26:23 +0100

    Add assets for landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,10 +1,56 @@
 import React from 'react'
 import ClubSlider from "./ClubSlider";
+import strefaKlubowa from '../static/img/strefa-klubowa.png'
+import strefaZawodnika from '../static/img/strefa-zawodnika.png'
+import tloTrenera from '../static/img/tlo-trenera.png'
+import tloZawodnikow from '../static/img/tlo-zawodnikow.png'
+import trener from '../static/img/trener.png'
+import zawodnicy from '../static/img/zawodnicy.png'
+import zoltaRamka from '../static/img/zolta-ramka.png'
+import tloStrefaZawodnika from '../static/img/tlo-strefy-zawodnika.png'
 
 const LandingPage = () => {
     return <main className="landingPage">
         <main className="landingPage__inner">
+            <img className="landingPage__img"
+                 id="yellowLines"
+                 src={zoltaRamka}
+                 alt="ramka" />
+            <a className="landingPage__left" href="#">
+                <img className="landingPage__img"
+                     id="zawodnikTlo"
+                     src={tloZawodnikow}
+                     alt="tlo-zawodnikow" />
+                <img className="landingPage__img"
+                    id="zawodnik"
+                    src={zawodnicy}
+                    alt="strefa-zawotnika" />
 
+                <img className="landingPage__img"
+                    id="strefaZawodnikaTlo"
+                    src={tloStrefaZawodnika}
+                     alt="strefa-zawotnika" />
+                <img className="landingPage__img"
+                     id="strefaZawodnika"
+                     src={strefaZawodnika}
+                     alt="strefa-zawodnika" />
+            </a>
+            <a className="landingPage__right" href="#">
+                <img className="landingPage__img"
+                     id="trenerTlo"
+                     src={tloTrenera}
+                     alt="tlo-trenera" />
+                <img className="landingPage__img"
+                     id="trener"
+                     src={trener}
+                     alt="strefa-klubu" />
+
+                <div id="tloStrefaKlubowa"></div>
+                <img className="landingPage__img"
+                     id="strefaKlubowa"
+                     src={strefaKlubowa}
+                     alt="strefa-klubowa" />
+            </a>
         </main>
 
         <ClubSlider />
